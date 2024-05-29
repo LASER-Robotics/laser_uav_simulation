d2e3b977fc02d0dd1a988cde0333d35fec390ae7
limao <rafael.novack07@gmail.com>
2024-05-29 10:17:40 -0300

    deleting old scripts

--- scripts/link_x500_model.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get the path to this script
-MY_PATH="$(dirname "$(dirname "$(realpath "$0")")")"
-SIMULATION_PATH=~/git/laser_uav_system/ros_packages/PX4-Autopilot/Tools/simulation/gz/models
-
-cd "$SIMULATION_PATH"
-ln -fs "$MY_PATH"/models/x500_v2 ./
