8b9a1875b7d7114ba65a716b448d9b6e20e677d4
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-12 14:04:05 -0300

    fix build

--- install.sh
+++ install.sh
@@ -7,11 +7,8 @@ sudo apt install ros-humble-gazebo-*
 cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts
 
 ./add_new_model.sh
-./link_model.sh
-./link_world.sh
-
-cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/third_party
-gitman install
+./link_models.sh
+./link_worlds.sh
 
 cd ~/laser_uav_system_ws/
 colcon build --symlink-install --packages-select realsense_ros realsense_gazebo_plugin
