1bccfbdfc2f7d73a5907ffd21c38769d0c33498a
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-18 16:05:16 -0300

    fix -y in apt install

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install ros-humble-realsense2-*
-sudo apt install ros-humble-librealsense2-*
-sudo apt install ros-humble-gazebo-*
+sudo apt-get install ros-humble-realsense2-* -y
+sudo apt install ros-humble-librealsense2-* -y
+sudo apt install ros-humble-gazebo-* -y
 
 cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts
 
