aab34507d0b73ef861d00959ccbbdb85ecad0347
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-26 16:49:52 -0300

    changes for final adjustment for production

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/simulation_core/scri
 ./link_worlds.sh
 
 cd ~/laser_uav_system_ws/
-colcon build --symlink-install --packages-select realsense2_camera realsense2_camera_msgs realsense2_description realsense_gazebo_plugin simulation_core
+colcon build --symlink-install --packages-select realsense2_camera realsense2_camera_msgs realsense2_description realsense_gazebo_plugin laser_uav_simulation
