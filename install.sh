57ab0922a92bfbfd066887af234aaa5581d21906
wagnerdgarcia <wagnergarcia@eng.ci.ufpb.com>
2025-05-28 14:00:46 -0300

    fix: Clean up .gitman.yml links and update install.sh for realsense dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install ros-humble-realsense2-*
-sudo apt install ros-humble-librealsense2-*
 sudo apt install ros-humble-gazebo-*
 
 cd third_party/livox_sdk
@@ -17,4 +15,4 @@ cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/core/scripts
 ./link_worlds.sh
 
 cd ~/laser_uav_system_ws/
-colcon build --symlink-install --packages-select realsense2_camera realsense2_camera_msgs realsense2_description realsense_gazebo_plugin laser_uav_simulation ros2_livox_simulation livox_sdk2 livox_ros_driver2
+colcon build --symlink-install --packages-select realsense_gazebo_plugin laser_uav_simulation ros2_livox_simulation livox_sdk2 livox_ros_driver2
