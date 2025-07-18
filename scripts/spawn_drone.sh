828ec3d5b94befeccc889c9fd6c7886515809c4a
Augusto Vinicius <augustoviniciud@gmail.com>
2025-07-18 13:10:35 -0300

    change the capslock vars env

--- scripts/spawn_drone.sh
+++ scripts/spawn_drone.sh
@@ -8,31 +8,31 @@ fi
 
 uavs_available=("x500" "minidrone")
 
-if [[ ! " ${uavs_available[@]} " =~ " $uav_type " ]]; then
-    echo "The UAV type '$uav_type' is not available."
+if [[ ! " ${uavs_available[@]} " =~ " $UAV_TYPE " ]]; then
+    echo "The UAV type '$UAV_TYPE' is not available."
     exit 1
 fi
 
-instance=$(echo "$uav_name" | sed -n 's/^uav\([0-9]\+\)$/\1/p')
+instance=$(echo "$UAV_NAME" | sed -n 's/^uav\([0-9]\+\)$/\1/p')
 instance=$((instance - 1))
 
-~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts/jinja_gen.py --namespace $uav_name --uav_model $uav_type --instance $instance $uav_sensors
+~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts/jinja_gen.py --namespace $UAV_NAME --uav_model $UAV_TYPE --instance $instance $UAV_SENSORS
 
 echo "Waiting for gazebo run."
 while ! pgrep -x "gzserver" > /dev/null; do
     sleep 1
 done
 sleep 5
-echo "Spawning the uav $uav_type with namespace $uav_name and sensors: $uav_sensors."
+echo "Spawning the uav $UAV_TYPE with namespace $UAV_NAME and sensors: $UAV_SENSORS."
 echo "In Position x: $1, y: $2, z: $3, orientation: $4."
 
-while gz model --verbose --spawn-file="/tmp/laser_uavs_description/sdf/${uav_type}_$instance.sdf" --model-name=${uav_name}_${uav_type}_$5 -x $1 -y $2 -z $3 -Y $4 | grep -q "An instance of Gazebo is not running."; do
+while gz model --verbose --spawn-file="/tmp/laser_uavs_description/sdf/${UAV_TYPE}_$instance.sdf" --model-name=${UAV_NAME}_${UAV_TYPE}_$5 -x $1 -y $2 -z $3 -Y $4 | grep -q "An instance of Gazebo is not running."; do
 	echo "gzserver not ready yet, trying again!"
 	sleep 1
 done
 
-export PX4_UXRCE_DDS_NS=$uav_name
-export PX4_SIM_MODEL=$uav_type
+export PX4_UXRCE_DDS_NS=$UAV_NAME
+export PX4_SIM_MODEL=$UAV_TYPE
 
 cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/ROMFS/
 
