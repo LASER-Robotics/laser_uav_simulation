b07ffb7599688c6c80b2b3fab72fdbda6e9d92c2
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-14 15:55:41 -0300

    change the way to spawn drones

---
+++ launch/spawn_drones.launch.py
@@ -0,0 +1,100 @@
+import launch
+import yaml
+
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def launch_setup(context: launch.LaunchContext, ld):
+    # #{ spawn drone config
+
+    config_file_path = LaunchConfiguration('spawn_drones_file').perform(context)
+
+    # #}
+
+    # #{ create the cmd lines with arguments for spawn drones
+    process_actions = []
+    
+    spawn_script_path = PathJoinSubstitution([FindPackageShare('laser_uav_simulation'), 'scripts', 'spawn_drone.sh']).perform(context)
+    uxrce_script_path = PathJoinSubstitution([FindPackageShare('laser_uav_simulation'), 'scripts', 'start_uxrce_protocol.sh']).perform(context)
+
+    try:
+        with open(config_file_path, 'r') as file:
+            config_data = yaml.safe_load(file)['/**']
+    except EnvironmentError:
+        print(f"Error: Can't find the config file in '{config_file_path}'")
+        return None
+    except KeyError:
+        print(f"Error: The key '/**' don't exist in YAML.")
+        return None
+
+    uavs = config_data.get('ros__parameters', {})
+
+    uavs_available = ['x500', 'lr7pro']
+
+    for uav in uavs:
+        id = uav.get('id', 0)
+        if id <= 0:
+            continue
+        namespace = "uav" + str(id)
+        uav_type = uav.get('type', '')
+        if not uav_type in uavs_available:
+            continue
+        pose_spawn = uav.get('pose_spawn', [])
+        pose_spawn = [str(i) for i in pose_spawn]
+        if len(pose_spawn) != 4:
+            continue
+        sensors = uav.get('sensors', [])
+
+        cmd_line = [spawn_script_path, namespace, uav_type]
+        for i in pose_spawn:
+            cmd_line.append(i)
+        for i in sensors:
+            cmd_line.append(i)
+
+        spawn_script_cmd = ExecuteProcess(
+            cmd=cmd_line,
+            name="spawn_drone_" + namespace, 
+            output='screen'
+        )
+        
+        process_actions.append(spawn_script_cmd)
+
+    # #}
+
+    # #{ start uxrce protocol
+        uxrce_script_cmd = ExecuteProcess(
+            cmd=[uxrce_script_path],
+            name="uxrce_protocol",
+            output='screen'
+        )
+        process_actions.append(uxrce_script_cmd)
+    # #}
+
+    for action in process_actions:
+        ld.add_action(action)
+
+def generate_launch_description():
+    ld = launch.LaunchDescription()
+
+    # #{ spawn drone config
+
+    ld.add_action(DeclareLaunchArgument(
+        'spawn_drones_file',
+        default_value='',
+        description='Path to config file for spawn drones.'
+    ))
+
+    # #}
+
+    # #{ opaque function
+
+    ld.add_action(
+        OpaqueFunction(function=launch_setup, args=[ld])
+    )
+
+    # #}
+
+    return ld
--- scripts/spawn_drone.sh
+++ scripts/spawn_drone.sh
@@ -1,38 +1,25 @@
 #!/usr/bin/env bash
 
-if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
-    echo "Error: the first 4 parameters are required!"
-    echo "The parameters, in order, are: x, y, z, and yaw."
-    exit 1
-fi
-
-uavs_available=("x500" "lr7pro")
-
-if [[ ! " ${uavs_available[@]} " =~ " $UAV_TYPE " ]]; then
-    echo "The UAV type '$UAV_TYPE' is not available."
-    exit 1
-fi
-
-instance=$(echo "$UAV_NAME" | sed -n 's/^uav\([0-9]\+\)$/\1/p')
+instance=$(echo "$1" | sed -n 's/^uav\([0-9]\+\)$/\1/p')
 instance=$((instance - 1))
 
-~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts/jinja_gen.py --namespace $UAV_NAME --uav_model $UAV_TYPE --instance $instance --x $1 --y $2 --z $3 --yaw $4 $UAV_SENSORS
+~/git/laser_uav_system/ros_packages/laser_uav_simulation/scripts/jinja_gen.py --namespace $1 --uav_model $2 --instance $instance --x $3 --y $4 --z $5 --yaw $6 $7
 
 echo "Waiting for gazebo run."
 while ! pgrep -x "gzserver" > /dev/null; do
     sleep 1
 done
 sleep 5
-echo "Spawning the uav $UAV_TYPE with namespace $UAV_NAME and sensors: $UAV_SENSORS."
-echo "In Position x: $1, y: $2, z: $3, orientation: $4."
+echo "Spawning the uav $2 with namespace $1 and sensors: $7."
+echo "In Position x: $3, y: $4, z: $5, orientation: $6."
 
-while gz model --verbose --spawn-file="/tmp/laser_uavs_description/sdf/${UAV_TYPE}_$instance.sdf" --model-name=${UAV_NAME}_${UAV_TYPE}_$5 -x $1 -y $2 -z $3 -Y $4 | grep -q "An instance of Gazebo is not running."; do
+while gz model --verbose --spawn-file="/tmp/laser_uavs_description/sdf/$2_$instance.sdf" --model-name=$1_$2_$instance -x $3 -y $4 -z $5 -Y $6 | grep -q "An instance of Gazebo is not running."; do
 	echo "gzserver not ready yet, trying again!"
 	sleep 1
 done
 
-export PX4_UXRCE_DDS_NS=$UAV_NAME
-export PX4_SIM_MODEL=$UAV_TYPE
+export PX4_UXRCE_DDS_NS=$1
+export PX4_SIM_MODEL=$2
 
 cd ~/git/laser_uav_system/ros_packages/laser_uav_simulation/ROMFS/
 
