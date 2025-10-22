867c98040b5f34770c319fbf7e77882f96903108
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-22 16:12:49 -0300

    fix

--- scripts/build_px4_firmware.sh
+++ scripts/build_px4_firmware.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd ~/git/laser_uav_system/ros_packages/px4_firmware
-# make distclean
-# make px4_sitl_default 
+make distclean
+make px4_sitl_default 
 cd build/px4_sitl_default/build_gazebo-classic
 cmake ../../../Tools/simulation/gazebo-classic/sitl_gazebo-classic
 make
