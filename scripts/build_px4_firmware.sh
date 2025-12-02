ee1904cfcc1c2ec31d0ae78a13afa3ffed2e1339
Augusto Vinicius <augustoviniciud@gmail.com>
2025-12-02 20:25:55 -0300

    Update build script to handle GitHub Actions

--- scripts/build_px4_firmware.sh
+++ scripts/build_px4_firmware.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-cd ~/git/laser_uav_system/ros_packages/px4_firmware
+if [ "${GITHUB_ACTIONS}" == "true" ]; then
+    BASE_DIR="$GITHUB_WORKSPACE"
+else
+    BASE_DIR="$(pwd)"
+fi
+
+cd $BASE_DIR/git/laser_uav_system/ros_packages/px4_firmware
 make distclean
 make px4_sitl_default 
 cd build/px4_sitl_default/build_gazebo-classic
